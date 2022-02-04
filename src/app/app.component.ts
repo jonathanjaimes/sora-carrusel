375d6b5f964cfcf5ff033c89214dfe417ec87208
Jonathan Jaimes <jonathanguillermojm@ufps.edu.co>
2022-02-04 15:59:59 -0500

    primero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'carrusel';
+  lista:any[] = [];
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['Anterior', 'Siguiente'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
+  constructor(private _http:HttpClient){
+    this.buscarImagen()
+  }
+
+  buscarImagen() {
+    this._http.get('https://jsonplaceholder.typicode.com/albums/1/photos?_start=0&_limit=5')
+    .subscribe((data:any) => {
+        this.lista = data
+        console.log(this.lista)
+    })
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppBrowserModule } from './app/app.browser.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppBrowserModule)
   .catch(err => console.error(err));
